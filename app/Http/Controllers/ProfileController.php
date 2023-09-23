2976196cfbf33772ebbaedf9747abaa67092a8d3
Aziz Arif Rizaldi <azizarifrizaldi@gmail.com>
2023-09-23 23:27:16 +0700

    Feat: Tambah parsing profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,6 +14,11 @@ class ProfileController extends Controller
         $data['desc_one']       = "Full Stack Developer - GITS.ID";
         $data['desc_two']       = "Studi Independen Batch 5";
         $data['mentor']         = "Muhammad Rizki Alif Pramanarasa";
+        $data['whatsapp_url']   = "https://api.whatsapp.com/send?phone=6285155336568&text=Hai%20Aziz";
+        $data['github_name']   = "@azizarizaldi";
+        $data['github_url']    = "https://github.com/azizarizaldi";
+        $data['twitter_name']   = "@azizarizaldi";
+        $data['twitter_url']    = "https://twitter.com/azizarizaldi";        
         return view('pages.profile', compact('data'));
     }
 }
