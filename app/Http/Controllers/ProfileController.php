b894042a3a1840c4d53f0acf6ec38df542e76e03
Aziz Arif Rizaldi <azizarifrizaldi@gmail.com>
2023-09-23 23:22:09 +0700

    Feat: Tambah Halaman Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,14 @@ class ProfileController extends Controller
 {
     public function index()
     {
-        $data['name'] = "Profil";
+        $data['name']           = "Aziz Arif Rizaldi";
+        $data['email']          = "azardi.business@gmail.com";
+        $data['university']     = "Sekolah Tinggi Teknologi Bandung";
+        $data['major']          = "Teknik Informatika";
+        $data['address']        = "Bandung, Jawa Barat, Indonesia.";
+        $data['desc_one']       = "Full Stack Developer - GITS.ID";
+        $data['desc_two']       = "Studi Independen Batch 5";
+        $data['mentor']         = "Muhammad Rizki Alif Pramanarasa";
         return view('pages.profile', compact('data'));
     }
 }
